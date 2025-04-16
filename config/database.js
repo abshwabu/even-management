b1243ef6c89b243b1629aa8f1e29efb30ce9b940
abdulaziz <abdulazizredi90@gmail.com>
2025-04-16 17:26:28 +0300

    db

--- config/database.js
+++ config/database.js
@@ -2,9 +2,22 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.PG_URI, {
+// Check if PG_URI is defined
+const pgUri = process.env.PG_URI;
+if (!pgUri) {
+    console.error('PG_URI is not defined in environment variables');
+    process.exit(1);
+}
+
+const sequelize = new Sequelize(pgUri, {
     dialect: 'postgres',
     logging: false,
+    dialectOptions: {
+        ssl: process.env.NODE_ENV === 'production' ? {
+            require: true,
+            rejectUnauthorized: false
+        } : false
+    }
 });
 
 export default sequelize;
